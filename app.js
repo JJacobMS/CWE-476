1b45d17888107095ef452603cc338e8bda272d81
JJacobMS <montielsalasjesus@gmail.com>
2024-10-10 13:44:50 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended : true }))
+
+app.post('/alumno',(req, res) => {
+    if(req.body.id){
+        res.send(`Hola ${alumnos[req.body.id - 1].nombre} ${alumnos[req.body.id - 1].apellido}`)
+    } else {
+        res.send("No se ha especificado el id del alumno.")
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`Aplicacion de ejemplo escuchando en el puerto ${port}`)
+})
+
+const alumnos = [
+    { nombre:"Pedro", apellido:"Perez" },
+    { nombre:"Rosa", apellido:"Vazquez" },
+    { nombre:"Susana", apellido:"Ochia" }
+];
